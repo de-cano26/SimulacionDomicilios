36b8e563f115ebe6ec4c5f9663328bb00574d1a9
David Cano <davidcano@nebular.co>
2012-11-30 18:09:53 -0500

    Andres 1

--- DelOesteDeliveries/src/com/deloeste/deliveries/dto/Delivery.java
+++ DelOesteDeliveries/src/com/deloeste/deliveries/dto/Delivery.java
@@ -2,18 +2,44 @@ package com.deloeste.deliveries.dto;
 
 import java.util.Date;
 
+
 /**
  * Class that represents an order
  */
-public class Delivery
-{
-    private Date datetime;
-    
-    private int sector;
-    
-    private int state;
-    
-   //Location to deliver
-    
-    //State
+public class Delivery {
+
+	public static final int ACTIVE = 1;
+
+	private Date datetime;
+
+	private int sector;
+
+	private int state;
+
+	public Delivery( Date datetime, int sector ) {
+		this.datetime = datetime;
+		this.sector = sector;
+		this.state = ACTIVE;
+	}
+
+	@Override
+	public String toString( ) {
+		return datetime.toString( ) + " - " + sector + " - " + state;
+	}
+
+	public int getState( ) {
+		return state;
+	}
+
+	public void setState( int state ) {
+		this.state = state;
+	}
+
+	public Date getDatetime( ) {
+		return datetime;
+	}
+
+	public int getSector( ) {
+		return sector;
+	}
 }
